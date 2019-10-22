f13649b0387d099d46659b059dfb39f54188e012
Omer Wazir <omer.wazir@rangle.io>
2019-10-22 13:12:17 -0400

    Add Andrea Leopardi list parser

---
+++ lib/list_parser.ex
@@ -0,0 +1,8 @@
+defmodule ListParser do
+  @spec parse(binary) :: list
+  def parse(str) do
+    {:ok, tokens, _} = str |> to_charlist() |> :list_lexer.string()
+    {:ok, list} = :list_parser.parse(tokens)
+    list
+  end
+end
