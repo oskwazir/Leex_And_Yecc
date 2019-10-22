5ba47d0c32bc53edc2ebc83bc4d672e2981e3ed4
Omer Wazir <omer.wazir@rangle.io>
2019-10-22 13:04:27 -0400

    Initial Commit

---
+++ lib/leex_and_yecc.ex
@@ -0,0 +1,18 @@
+defmodule LeexAndYecc do
+  @moduledoc """
+  Documentation for LeexAndYecc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LeexAndYecc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/leex_and_yecc_test.exs
@@ -0,0 +1,8 @@
+defmodule LeexAndYeccTest do
+  use ExUnit.Case
+  doctest LeexAndYecc
+
+  test "greets the world" do
+    assert LeexAndYecc.hello() == :world
+  end
+end
